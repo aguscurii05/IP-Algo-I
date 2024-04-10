cfc0e5dc26d4f4382ffc32f9544719a1ebd586da
unknown <agustinstescovichcuri@gmail.com>
2024-04-10 16:55:59 -0300

    act

--- Ej2.hs
+++ Ej2.hs
@@ -42,3 +42,39 @@ ambosSon0' x y = False
 
 --ej2.f
 
+mismoIntervalo :: Float->Float->Bool
+mismoIntervalo x y| x<=3 && y<=3 = True
+                  | 3<x && x<=7 && 3<y && y<=7 = True
+                  | 7<=x && 7<=y = True
+                  | otherwise = False
+
+--ej2.g
+
+sumaDistintos::Int->Int->Int->Int
+sumaDistintos x y z| x==y && y==z = 0
+                   | x==y = z
+                   | y==z = x
+                   | x==z = y
+                   | otherwise = x+y+z
+
+--ej2.h
+
+divisible::Int->Int->Bool
+divisible x y| (mod x y)==0 =True 
+             | otherwise = False
+
+esMultiploDe::Int->Int->Bool
+esMultiploDe x y|x>0 && y>0 && (divisible x y)==True = True
+                |otherwise = False
+
+--ej2.i
+
+digitoUnidades::Int->Int
+digitoUnidades x| x>9 = (x-((div x 10)*10))
+                | otherwise = x
+
+--ej2.j
+
+digitoDecenas:: Int->Int
+digitoDecenas x| x>9 = div x 10
+               | otherwise = 0
