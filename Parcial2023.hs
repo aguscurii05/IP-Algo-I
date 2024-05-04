d5d41637fd70f58a453b282a2cf2743beefc3375
unknown <agustinstescovichcuri@gmail.com>
2024-05-04 10:41:50 -0300

    parciales

--- Parcial2023.hs
+++ Parcial2023.hs
@@ -7,6 +7,38 @@ equiposValidos [(x1,x2)] = True
 equiposValidos [(x1,x2),(y1,y2)] = x1/=y1 && x1/=y2 && x2/=y2
 equiposValidos ((x1,x2):(y1,y2):xs) = (x1/=y1 && x1/=y2) && x2/=y2 && equiposValidos ((x1,x2):xs) && equiposValidos ((y1,y2):xs)
 
-jugadoresRepetidos:: [(String,String)]->Bool
-jugadoresRepetidos [(x1,x2),(y1,y2)] = x2==y2
-jugadoresRepetidos ((x1,x2):(y1,y2):xs) = x2==y2 || jugadoresRepetidos ((x1,x2):xs) || jugadoresRepetidos ((y1,y2):xs)
+sumatoria::[Integer]->Integer
+sumatoria [x] = x
+sumatoria (x:xs) = x + sumatoria xs
+
+atajaronSuplentes::[(String,String)]->[Integer]->Integer->Integer
+atajaronSuplentes [] [] totGoles = totGoles
+atajaronSuplentes arquerosPorEquipo goles totGoles = totGoles -sumatoria goles
+
+posicionDeN::(Eq t)=>t->[(t,t)]->Integer
+posicionDeN n ((x1,x2):xs) | n==x2 = 1
+                               | otherwise = 1 + posicionDeN n xs
+
+posicionDeN2::(Eq t)=>t->[t]->Integer
+posicionDeN2 n (x:xs) | n==x = 1
+                               | otherwise = 1 + posicionDeN2 n xs
+
+    
+
+elementoN::(Eq t)=>Integer->[t]->t
+elementoN n (x:xs)| n==1 = x
+                  | otherwise = elementoN (n-1) (xs)
+
+
+
+porcentajeDeGoles::String->[(String,String)]->[Integer]->Float
+porcentajeDeGoles arquero arquerosPorEquipo goles = 100* (fromIntegral (elementoN (posicionDeN arquero arquerosPorEquipo) goles) / fromIntegral (sumatoria goles))
+
+menorElemento::[Integer]->Integer
+menorElemento [x] =x
+menorElemento [x,y] | x<=y = x
+                    | otherwise = y
+menorElemento (x:xs) | x<= head xs = menorElemento (x:tail xs)
+
+vallaMenosVencida::[(String,String)]->[Integer]->String
+vallaMenosVencida arquerosPorEquipo goles = snd(elementoN (posicionDeN2 (menorElemento goles) goles) arquerosPorEquipo)
--- testingParcial2023.hs
+++ testingParcial2023.hs
@@ -16,3 +16,39 @@ testingEquiposValidos = test [
 
  ]
 
+testingAtajaronSuplentes = test [
+    "caso1" ~: atajaronSuplentes [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [4,8,4,9,6] 40 ~?= 9,
+    "caso1" ~: atajaronSuplentes [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [4,8,0,0,0] 40 ~?= 28,
+    "caso1" ~: atajaronSuplentes [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [0,0,0,0,0] 40 ~?= 40,
+    "caso1" ~: atajaronSuplentes [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [0,0,0,0,0] 0 ~?= 0,
+    "caso2" ~: atajaronSuplentes [] [] 25 ~?= 25
+ ]
+
+posicionDeNTesting = test [
+    "caso1" ~: posicionDeN "Messi" [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] ~?= 2,
+    "caso1" ~: posicionDeN "Armani" [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] ~?= 1,
+    "caso1" ~: posicionDeN "Torrico" [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] ~?= 4,
+    "caso1" ~: posicionDeN "Neuer" [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] ~?= 5
+
+ ]
+
+elementoNTesting = test [
+    "caso1" ~: elementoN 5 [2,3,4,6,4,6,5] ~?= 4,
+    "caso1" ~: elementoN 2 [2,3,4,6,4,6,5] ~?= 3,
+    "caso1" ~: elementoN 6 [2,3,4,6,4,6,5] ~?= 6,
+    "caso1" ~: elementoN 1 [2,3,4,6,4,6,5] ~?= 2
+ ]
+
+porcentajeDeGolesTesting = test [
+    "caso1" ~: porcentajeDeGoles "Neuer" [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [4,8,4,4,5] ~?= 20.0,
+    "caso1" ~: porcentajeDeGoles "Dibu" [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [4,8,4,4,5] ~?= 16.0
+
+ ]
+
+vallaMenosVencidaTesting = test [
+    "caso1" ~: vallaMenosVencida [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [4,8,4,9,6] ~?= "Armani",
+    "caso2" ~: vallaMenosVencida [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [4,8,4,9,1] ~?= "Neuer",
+    "caso3" ~: vallaMenosVencida [("River","Armani"),("Boca","Messi"),("Racing","Dibu"),("San Lorenzo","Torrico"),("Independiente","Neuer")] [1,1,1,1,1] ~?= "Armani",
+    "caso4" ~: vallaMenosVencida [("Barca","Ter Stegen")] [5] ~?= "Ter Stegen"
+
+ ]
